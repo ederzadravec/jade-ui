85efa786b9a94eca9ecb1a2a7c7c7aea3de00817
Éder Freitas <ederzadravec@gmail.com>
2020-10-16 16:57:23 -0300

    docs

--- src/Playground.jsx
+++ src/Playground.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Playground } from 'docz';
 import { Provider } from './Components/Provider/Provider';
+import { createGlobalStyle } from 'styled-components';
 
 export default ({ ...props }) => {
+  const GlobalStyle = createGlobalStyle`
+    * {
+      box-sizing: border-box;
+    }
+  `;
+
   return (
-    <Provider noDefaultCss>
-      <Playground {...props} />
-    </Provider>
+    <>
+      <GlobalStyle />
+      <Provider noDefaultCss>
+        <Playground {...props} />
+      </Provider>
+    </>
   );
 };
