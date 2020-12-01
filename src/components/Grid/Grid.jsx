ef718350d1ef2a778bf5553f3ec3631e9125692a
Diego <ddzadravec@gmail.com>
2020-12-01 20:43:31 -0300

    Atualizando Grid

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -1,23 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
-const Container = styled.section`
+import { Space } from '../Space/Space';
+
+const Container = styled(Space)`
   display: flex;
   flex-direction: row;
-  width: 100%;
+  width: calc(100% + ${({ spacing }) => spacing}px);
   flex-wrap: wrap;
+  justify-content: flex-start;
+  margin-left: -${({ spacing }) => spacing / 2}px;
+  margin-right: -${({ spacing }) => spacing / 2}px;
+  transition: all ease 0.1s;
 `;
 
-const Content = styled.div`
-  height: max-content;
-  ${({ theme, size }) => `
+const Content = styled(Space)`
+  display: flex;
+  flex-wrap: wrap;
+  transition: all ease 0.1s;
+
+  ${({ theme, size, spacing, direction }) => `
+    flex-direction: ${direction};
+    padding-left: ${spacing / 2}px;
+    padding-right: ${spacing / 2}px;
+    margin-bottom: ${spacing}px;
+
     width: ${(100 / 12) * (size.xl || size.lg || size.md || size.sm || size.xs || 12)}%;
 
     ${Object.keys(size)
       .map(key => theme.screens[key](`width:${(100 / 12) * size[key]}%`, size[key]))
       .join(';')}
-  `}
+  `};
 `;
 
 export const Grid = ({ container, children, ...props }) => {
@@ -29,17 +43,20 @@ export const Grid = ({ container, children, ...props }) => {
 };
 
 Grid.defaultProps = {
+  spacing: 24,
   container: false,
   size: { md: 12 },
+  direction: 'row',
 };
 
-Grid.propTypes = {
-  container: PropTypes.bool,
-  size: PropTypes.shape({
-    xs: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
-    sm: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
-    md: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
-    lg: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
-    xl: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
-  }),
-};
+// Grid.propTypes = {
+//   spacing: PropTypes.number,
+//   container: PropTypes.bool,
+//   size: PropTypes.shape({
+//     xs: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
+//     sm: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
+//     md: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
+//     lg: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
+//     xl: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
+//   }),
+// };
